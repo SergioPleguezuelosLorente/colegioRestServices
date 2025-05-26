3840e6eac9fd4c07af1e65dcb5c7a85cf6e59e96
SergioPleguezuelosLorente <sergio.p.lorente@gmail.com>
2025-05-26 12:30:53 +0200

    comsuming rest service saveMsg

--- controller/ContactController.java
+++ controller/ContactController.java
@@ -1,11 +1,15 @@
 package com.escuela.controller;
 
 import com.escuela.model.Contact;
+import com.escuela.model.Response;
 import com.escuela.proxy.ContactProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,10 +19,22 @@ public class ContactController {
     @Autowired
     ContactProxy contactProxy;
 
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status") String status) {
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact) {
+        String link = "http://localhost:8080/api/contact/saveMsg";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("invocationFrom", "RestTemplate");
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact, headers);
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(link, HttpMethod.POST, httpEntity, Response.class);
+        return responseEntity;
     }
 
 }
